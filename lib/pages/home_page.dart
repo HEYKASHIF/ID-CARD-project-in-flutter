36dd36bd82394afcd06a3c8fab20057b66369155
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-28 04:23:02 +0530

    28-march commit17

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,9 +7,14 @@ import 'package:flutter/material.dart';
 
 // }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
