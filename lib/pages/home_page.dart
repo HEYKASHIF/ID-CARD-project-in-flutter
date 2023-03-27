f2cd64b23174eb994529d346e9cbbb1b4e323891
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-27 15:48:26 +0530

    27-march commit13

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:card/main.dart';
+import 'package:flutter/material.dart';
+
+// void main() {
+
+//   runApp(MyApp());
+
+// }
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
