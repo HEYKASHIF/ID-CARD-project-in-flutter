64b4a10646f71025ed212c53323d7226123415b3
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-27 16:00:00 +0530

    27-march commit14

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,6 +12,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.amber,
+    );
   }
 }
